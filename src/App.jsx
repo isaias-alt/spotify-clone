446e3eba3b8ec45592be49e3546294e9c29cc18a
Lucas Isaias Casco <cascolucasisaias@gmail.com>
2023-04-14 11:54:36 -0400

    Add Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import Login from './components/Login'
 
 export default function App () {
   return (
     <div className="App">
-      hola mundo
+      <Login />
     </div>
   )
 }
