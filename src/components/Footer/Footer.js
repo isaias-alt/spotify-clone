85872e3842e2291b36b05fb4eb47adfa66f19818
Lucas Isaias Casco <cascolucasisaias@gmail.com>
2023-04-17 01:53:13 -0400

    style: :art: syling Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,52 @@
 import styled from 'styled-components'
 
 const FooterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
   position: fixed;
   bottom: 0;
-  height: 70px;
+  height: 90px;
   width: 100vw;
   background-color: #181818;
   color: #ffffff;
 
 `
 
-export { FooterContainer }
+const FooterLeft = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  max-width: 300px;
+  margin-left: 15px;
+  & img {
+    height: 60px;
+    width: 60px;
+    margin-right: 20px;
+    object-fit: contain;
+  }
+`
+
+const FooterCenter = styled.div`
+  flex: 0.4;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 300px;
+`
+
+const FooterRight = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  margin-right: 20px;
+  & .MuiSlider-root {
+    width: 100px;
+    color: #FFFFFF;
+  }
+
+  & .MuiSlider-root:hover {
+    color: #1ED760;
+  }
+`
+
+export { FooterContainer, FooterLeft, FooterCenter, FooterRight }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
-import { FooterContainer } from './Footer'
+import { FooterCenter, FooterContainer, FooterLeft, FooterRight } from './Footer'
+import ShuffleIcon from '@mui/icons-material/Shuffle'
+import SkipPreviousIcon from '@mui/icons-material/SkipPrevious'
+import PlayCircleIcon from '@mui/icons-material/PlayCircle'
+import SkipNextIcon from '@mui/icons-material/SkipNext'
+import RepeatIcon from '@mui/icons-material/Repeat'
+import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay'
+import VolumeDownIcon from '@mui/icons-material/VolumeDown'
+import { Grid, Slider } from '@mui/material'
 
 const Footer = () => {
   return (
-    <FooterContainer>Footer</FooterContainer>
+    <FooterContainer>
+      <FooterLeft>
+        <img src="https://static.posters.cz/image/750/buque-costero/coldplay-parachutes-album-cover-i56853.jpg" alt="cover" />
+        <div>
+          <h4>Shape of My Heart</h4>
+          <p>Sting</p>
+        </div>
+      </FooterLeft>
+      <FooterCenter>
+        <ShuffleIcon />
+        <SkipPreviousIcon />
+        <PlayCircleIcon />
+        <SkipNextIcon />
+        <RepeatIcon />
+      </FooterCenter>
+      <FooterRight>
+        <Grid container spacing={2}>
+        <Grid item> <PlaylistPlayIcon /> </Grid>
+        <Grid item> <VolumeDownIcon />  </Grid>
+        <Grid item xs> <Slider size="small"/> </Grid>
+        </Grid>
+      </FooterRight>
+    </FooterContainer>
   )
 }
 
