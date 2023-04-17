3c83eeb9dabad0386dc77e73e63178dc4cefa1d1
Lucas Isaias Casco <cascolucasisaias@gmail.com>
2023-04-17 01:54:05 -0400

    Add Header

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
 const BodyContainer = styled.div`
-  flex: 0%.8;
+  flex: 0.8;
+  background-color: #121212;
 `
 
 export { BodyContainer }
--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { BodyContainer } from './Body'
+import Header from './Header.jsx'
 
 const Body = () => {
   return (
-    <BodyContainer>Body</BodyContainer>
+    <BodyContainer>
+      <Header />
+    </BodyContainer>
   )
 }
 
