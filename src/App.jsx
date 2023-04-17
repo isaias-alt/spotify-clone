c227cc4d481d19dc7b41cb3ac3e55a56a8d463f9
Lucas Isaias Casco <cascolucasisaias@gmail.com>
2023-04-17 15:53:08 -0400

    Login autentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-// import Login from './components/Login/Login.jsx'
+import { useState, useEffect } from 'react'
+import Login from './components/Login/Login.jsx'
 import Player from './components/Player/Player.jsx'
+import { getTokenFromURL } from './spotifyLoginLogic.js'
 
 export default function App () {
+  const [token, setToken] = useState('')
+  useEffect(() => {
+    const hash = getTokenFromURL()
+    if (hash && hash.access_token) {
+      window.location.hash = ''
+      const _token = hash.access_token
+      setToken(_token)
+    }
+  }, [])
+
   return (
     <div className="App">
-      <Player />
-      {/* <Login /> */}
+      {
+        token ? <Player /> : <Login />
+      }
     </div>
   )
 }
---
+++ src/spotifyLoginLogic.js
@@ -0,0 +1,21 @@
+export const getTokenFromURL = () => {
+  return window.location.hash.substring(1).split('&')
+    .reduce((initial, item) => {
+      const parts = item.split('=')
+      initial[parts[0]] = decodeURIComponent(parts[1])
+      return initial
+    }, {})
+}
+
+const endpoint = 'https://accounts.spotify.com/authorize'
+const clientID = '971583d86df04596ad9f2e3757b81d66'
+const redirectUri = 'http://localhost:5173/'
+// const redirectURI = 'https://super-spotify-clone.vercel.app/'
+const scopes = [
+  'user-read-currently-playing',
+  'user-read-recently-played',
+  'user-read-playback-state',
+  'user-top-read',
+  'user-modify-playback-state'
+]
+export const LOGIN_URL = `${endpoint}?client_id=${clientID}&response_type=token&redirect_uri=${redirectUri}&scope=${scopes.join('%20')}&show_dialog=true`
