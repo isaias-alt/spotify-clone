6b10855ea515225125b7860daac379299ff58c9b
Lucas Isaias Casco <cascolucasisaias@gmail.com>
2023-04-17 23:50:00 -0400

    fixing Login bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import Login from './components/Login/Login.jsx'
 import Player from './components/Player/Player.jsx'
 import { getTokenFromURL } from './spotifyLoginLogic.js'
+import { SET_USER } from './features/UserSlice.js'
 
 export default function App () {
   const [token, setToken] = useState('')
+  const dispatch = useDispatch()
+  const user = 'javascript'
+
   useEffect(() => {
     const hash = getTokenFromURL()
     if (hash && hash.access_token) {
       window.location.hash = ''
       const _token = hash.access_token
-      setToken(_token)
+      dispatch(SET_USER({ user }))
     }
   }, [])
 
