02f4774829a37216899efe7b92d46e38654873a1
Lucas Isaias Casco <cascolucasisaias@gmail.com>
2023-04-18 23:10:10 -0400

    finishing the project <3

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,5 @@
 import { LoginButton, LoginContainer } from './Login'
-
-const endpoint = 'https://accounts.spotify.com/authorize'
-const clientID = '971583d86df04596ad9f2e3757b81d66'
-const redirectURI = 'https://super-spotify-clone.vercel.app/'
-const scopes = [
-  'user-read-currently-playing',
-  'user-read-recently-played',
-  'user-read-playback-state',
-  'user-top-read',
-  'user-modify-playback-state'
-]
-const LOGIN_URL = `${endpoint}?client_id=${clientID}&response_type=token&redirect_uri=${redirectURI}&scope=${scopes.join('%20')}&show_dialog=true`
+import { LOGIN_URL } from '../../spotifyLoginLogic.js'
 
 const Login = () => {
   return (
