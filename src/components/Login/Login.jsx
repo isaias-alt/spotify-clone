ae28a2b1da47fd7fcd30f43f31e1e1b5ad01a1a4
Lucas Isaias Casco <cascolucasisaias@gmail.com>
2023-04-18 23:07:06 -0400

    finishing the proyect <3

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,4 @@
 import { LoginButton, LoginContainer } from './Login'
-<<<<<<< HEAD
 
 const endpoint = 'https://accounts.spotify.com/authorize'
 const clientID = '971583d86df04596ad9f2e3757b81d66'
@@ -12,9 +11,6 @@ const scopes = [
   'user-modify-playback-state'
 ]
 const LOGIN_URL = `${endpoint}?client_id=${clientID}&response_type=token&redirect_uri=${redirectURI}&scope=${scopes.join('%20')}&show_dialog=true`
-=======
-import { LOGIN_URL } from '../../spotifyLoginLogic.js'
->>>>>>> dev
 
 const Login = () => {
   return (
